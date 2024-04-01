715cc570c05993e87a8f2ad86cf81f816563d23b
Jung-min Lee <starjungmin@gmail.com>
2024-04-01 20:15:35 +0900

    Hello world 3

--- main.py
+++ main.py
@@ -13,4 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Hello world 2')
+    print_hi('Hello world 3')
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
